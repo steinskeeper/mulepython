aa31115d2b48f24c5473b8c81da135ba45f24342
steinskeeper <dpelpsykongroo@gmail.com>
2022-04-04 07:35:29 +0530

    dbname

--- api/notmule.py
+++ api/notmule.py
@@ -3,45 +3,93 @@ from this import s
 from fastapi import APIRouter
 from fastapi import Request, FastAPI
 import pymongo
-from bson import ObjectId
+from bson import json_util, ObjectId
 import os
 from dotenv import load_dotenv
+import json
 notmule = APIRouter()
 
 load_dotenv()
-mongourl=os.getenv("MONGO")
+mongourl = os.getenv("MONGO")
 
 
 myclient = pymongo.MongoClient(
     mongourl,)
-db = myclient["dbdb"]
+db = myclient["db1"]
 col = db["orders"]
 
 
 @notmule.post("/await")
 async def aww(request: Request):
-    print(await request.json())
+    re = (await request.json())
+    print(re)
+    orders = []
+    dat = db.orders.find({"sellerid": re["sellerid"]})
+    dat_san = json.loads(json_util.dumps(dat))
 
-    orders=[]
-    for x in db.orders.find({"sellerid":sell},{'_id': 0}):
+    for x in dat_san:
         orders.append(x)
+
     for user in orders:
-        
-        buy = db.users.find_one({"_id": ObjectId(user["buyerid"])},{'_id': 0})
+
+        buy = db.users.find_one({"_id": ObjectId(user["buyerid"])}, {'_id': 0})
         user["buyerName"] = buy["username"]
         user["avatarURL"] = buy["avatarURL"]
     for item in orders:
-        listing = db.users.find_one({"_id": ObjectId(item["sellerid"]),"listings.id":item["listingid"]},{"listings.$":1,'_id': 0})
+        listing = db.users.find_one({"_id": ObjectId(
+            item["sellerid"]), "listings.id": item["listingid"]}, {"listings.$": 1, '_id': 0})
         item["title"] = listing["listings"][0]["oname"]
         item["price"] = listing["listings"][0]["oprice"]
-        print(listing)
 
     return orders
 
 
+@notmule.post("/previous-orders")
+async def prevv(request: Request):
+    re = (await request.json())
+    print(re)
+    orders = []
+    dat = db.orders.find({"buyerid": re["userid"]})
+    dat_san = json.loads(json_util.dumps(dat))
+
+    for x in dat_san:
+        orders.append(x)
+
+    for user in orders:
+
+        buy = db.users.find_one({"_id": ObjectId(user["buyerid"])}, {'_id': 0})
+        user["sellerName"] = buy["username"]
+
+    for item in orders:
+        listing = db.users.find_one({"_id": ObjectId(
+            item["sellerid"]), "listings.id": item["listingid"]}, {"listings.$": 1, '_id': 0})
+        item["title"] = listing["listings"][0]["oname"]
+        item["price"] = listing["listings"][0]["oprice"]
+        item["imgURL"] = listing["listings"][0]["opic"]
+
+    return orders
 
 
+@notmule.post("/orderstat")
+async def stat(request: Request):
+    re = (await request.json())
+    print(re)
+    db.orders.update_one({"_id": ObjectId(re["orderid"])}, {
+                         "$set": {"status": re["status"]}})
 
+    return {"message": "success"}
 
+@notmule.post("/teams")
+async def teams(request: Request):
+    re = (await request.json())
+    print(re)
+    teams = []
+    dat = db.users.find({"skill": (re["skill"])})
+    dat_san = json.loads(json_util.dumps(dat))
 
-    
+    for x in dat_san:
+        del x["password"]
+        teams.append(x)
+        
+
+    return teams
